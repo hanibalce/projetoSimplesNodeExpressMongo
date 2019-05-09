aeb9698c7c1213fc00e6f7bec578dd52bb227664
Anibal C Oliveira <hanibal.ce80@gmail.com>
2019-05-09 16:03:01 -0300

    Alteração das pastas.

--- ProjetoNode/app/routes/usuario.js
+++
@@ -1,7 +0,0 @@
-let controller = require('../controllers/usuario.js');
-
-module.exports = function(app){
-    app.get('/usuarios', controller.listarUsuario);
-    app.get('/usuarios/:id', controller.obterUsuario);
-    app.post('/usuarios', controller.inserirUsuario);
-}
